3e5853befafcbe3249e464b91aafbb4626cbe41c
thiyo12 <66420586+thiyo12@users.noreply.github.com>
2026-04-07 18:05:31 +0530

    Add code protection and security headers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,49 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: 'X-Download-Options',
+    value: 'noopen',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+]
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['res.cloudinary.com', 'localhost'],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  productionBrowserSourceMaps: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
   },
 }
 
