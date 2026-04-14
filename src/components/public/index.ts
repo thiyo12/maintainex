2a7a4ed44ef7ecdced33163dd3d3a8910b1d720f
thiyo12 <66420586+thiyo12@users.noreply.github.com>
2026-04-15 02:31:40 +0530

    Remove duplicate src folder, fix tsconfig paths

--- src/components/public/index.ts
+++
@@ -1,7 +0,0 @@
-export { Header } from "./Header";
-export { Footer } from "./Footer";
-export { Hero } from "./Hero";
-export { ProjectsGrid } from "./ProjectsGrid";
-export { Stats } from "./Stats";
-export { Testimonials } from "./Testimonials";
-export { HowItWorks } from "./HowItWorks";
